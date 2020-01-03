8966f2739f6fcb15d12098027b037fcf13b794f6
Candace Wilson <candacewilson@Candaces-MacBook-Air.local>
2020-01-03 13:56:18 -0500

    about page

---
+++ wheelgood/src/components/About/people.js
@@ -0,0 +1,11 @@
+ export const people = [
+    { id: 1, src: './images/brandon.jpg', name: 'Brandon Allison', github: 'BrandonJAllison'},
+    { id: 2, src: './images/evy.jpg', name: 'Eyvonne Geordan', github: 'Eyvonne'},
+    { id: 3, src: './images/jesse.jpg', name: 'Jesse Otradovec', github: 'JesseOtradovec'},
+    { id: 4, src: './images/mhart.jpg', name: 'Michael Hart', github: 'worksofhart'},
+    { id: 5, src: './images/candace.jpg', name: 'Candace Wilson ', github: 'Candaceyw'},
+    { id: 6, src: './images/mikio.jpg', name: 'Mikio Harman', github: 'mpharm88'},
+    { id: 6, src: './images/mtrew.jpg', name: 'Michael Trew', github: 'mphamtrew2015rm88'}
+
+];
+
--- wheelgood/src/components/App/App.js
+++ wheelgood/src/components/App/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavBar from '../NavBar';
-
 import './App.scss';
+
+import About from '../About'
 
 const App = (props) => {
 
 
-
-  return (
-    <Router>
-      <div className='App'>
-        <Switch>
-          <Route path='/' exact>
-            <NavBar />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+	return (
+		<Router>
+			<div className='App'>
+				<Switch>
+					<Route path='/' exact>
+						<NavBar />
+						<Map />
+						
+					</Route>
+				</Switch>
+			</div>
+		</Router>
+	);
 };
 
 export default App;
